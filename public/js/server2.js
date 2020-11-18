8f8398c2b9b7980b4f98b15a0ccbb8dac168126a
Lillian Paris <lillian.paris529@gmail.com>
2020-11-17 21:38:11 -0700

    Fixed travis errors

--- public/js/server2.js
+++ public/js/server2.js
@@ -1,28 +1,27 @@
-$(document).ready(function () {
-    let endpoint = 'https://auth.predicthq.com/token';
-    let token = 'NFlkQVQ2ZGE0QUE6OEF6VzZicWdTWUFMVHNJUWVoNnN4MGN3dnJHRk5nb3pEWE1hZlNELU55MF9hV19yMmduRGN3';
-    let accessToken = null
+$(document).ready(() => {
+  const endpoint = "https://auth.predicthq.com/token";
+  const token =
+    "NFlkQVQ2ZGE0QUE6OEF6VzZicWdTWUFMVHNJUWVoNnN4MGN3dnJHRk5nb3pEWE1hZlNELU55MF9hV19yMmduRGN3";
+  let accessToken = null;
+  $.ajax({
+    url: endpoint,
+    method: "POST",
+    headers: {
+      Authorization: "Basic " + token,
+      "Content-Type": "application/x-www-form-urlcoded",
+    },
+    data: "grant_type=client_credentials&scope=events",
+  }).then((response) => {
+    console.log(response);
+    accessToken = response.access_token;
     $.ajax({
-        url: endpoint,
-        method: "POST",
-        headers: {
-            'Authorization': 'Basic ' + token,
-            'Content-Type': 'application/x-www-form-urlcoded'
-            
-        },
-        data: 'grant_type=client_credentials&scope=events'
-    }).then(function (response) {
-        console.log(response); 
-        accessToken = response.access_token;
-        $.ajax({
-            url: 'https://api.predicthq.com/v1/events',
-            method: "GET",
-            headers: {
-                'Authorization': 'Bearer ' + accessToken,
-            }
-        })
-        .then(function (response) {
-            console.log(response);
-        })
-    })
+      url: "https://api.predicthq.com/v1/events",
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + accessToken,
+      },
+    }).then((response) => {
+      console.log(response);
+    });
+  });
 });
