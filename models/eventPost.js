cdb59e5832cf2d01aa6da338e98f782e60593dd6
Bryan Lilburn <bryanlilburn@gmail.com>
2020-11-19 21:16:29 -0800

    updated models with eventPost.js, updated controllers with eventPostsController.js, updated htmlController.js, updated controllers and routerin index.js, updated views with postedevents.handlebars

---
+++ models/eventPost.js
@@ -0,0 +1,28 @@
+module.exports = function(sequelize, DataTypes) {
+    var EventPost = sequelize.define("EventPost", {
+      event: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        len: [1]
+      }
+    });
+  
+    EventPost.associate = function(models) {
+      // We're saying that a Post should belong to an User
+      // A Post can't be created without an User due to the foreign key constraint
+      EventPost.belongsTo(models.User, {
+        foreignKey: {
+          allowNull: false
+        }
+      });
+    };
+  
+    return EventPost;
+  };
