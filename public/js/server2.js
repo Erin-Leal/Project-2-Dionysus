78e8915a0f4ff38e13547ebcf3bfd102ff8d0eb0
Lillian Paris <lillian.paris529@gmail.com>
2020-11-21 11:24:29 -0700

    Created places option to the api

--- public/js/server2.js
+++ public/js/server2.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   const endpoint = "https://auth.predicthq.com/token";
   const token =
-    "NFlkQVQ2ZGE0QUE6OEF6VzZicWdTWUFMVHNJUWVoNnN4MGN3dnJHRk5nb3pEWE1hZlNELU55MF9hV19yMmduRGN3";
+    "M1FCSGdRYWxvZ0U6VW1hRUxrUnYzZ0Q3TFdiV3pEYzZQb2JDdUVuWXJBX2JNdms1aklCb0dsLWRMa0tUaThwS3d3";
   let accessToken = null;
   $.ajax({
     url: endpoint,
@@ -10,18 +10,66 @@ $(document).ready(() => {
       Authorization: "Basic " + token,
       "Content-Type": "application/x-www-form-urlcoded",
     },
-    data: "grant_type=client_credentials&scope=events",
-  }).then((response) => {
-    console.log(response);
-    accessToken = response.access_token;
+    data: "grant_type=client_credentials&scope=places",
+  }).then((placeauthresponse) => {
+    // console.log(placeauthresponse);
+    accessToken = placeauthresponse.access_token;
     $.ajax({
-      url: "https://api.predicthq.com/v1/events",
+      url: "https://api.predicthq.com/v1/places/?q=Nottingham,England",
       method: "GET",
       headers: {
         Authorization: "Bearer " + accessToken,
       },
-    }).then((response) => {
-      console.log(response);
+    }).then((placesresponse) => {
+      $.ajax({
+        url: endpoint,
+        method: "POST",
+        headers: {
+          Authorization: "Basic " + token,
+          "Content-Type": "application/x-www-form-urlcoded",
+        },
+        data: "grant_type=client_credentials&scope=events",
+      }).then((eventsauthresponse) => {
+        accessToken = eventsauthresponse.access_token;
+        // console.log(eventsauthresponse);
+        $.ajax({
+          url: "https://api.predicthq.com/v1/events/?place.scope=5128638",
+          method: "GET",
+          headers: {
+            Authorization: "Bearer " + accessToken,
+          },
+        }).then((eventsresponse) => {
+          console.log(eventsresponse, placesresponse);
+        });
+      });
     });
   });
 });
+
+// $(document).ready(() => {
+//   const endpoint = "https://auth.predicthq.com/token";
+//   const token =
+//     "NFlkQVQ2ZGE0QUE6OEF6VzZicWdTWUFMVHNJUWVoNnN4MGN3dnJHRk5nb3pEWE1hZlNELU55MF9hV19yMmduRGN3";
+//   let accessToken = null;
+//   $.ajax({
+//     url: endpoint,
+//     method: "POST",
+//     headers: {
+//       Authorization: "Basic " + token,
+//       "Content-Type": "application/x-www-form-urlcoded",
+//     },
+//     data: "grant_type=client_credentials&scope=events",
+//   }).then((response) => {
+//     console.log(response);
+//     accessToken = response.access_token;
+//     $.ajax({
+//       url: "https://api.predicthq.com/v1/events/?place.scope=5128638",
+//       method: "GET",
+//       headers: {
+//         Authorization: "Bearer " + accessToken,
+//       },
+//     }).then((response) => {
+//       console.log(response);
+//     });
+//   });
+// });
